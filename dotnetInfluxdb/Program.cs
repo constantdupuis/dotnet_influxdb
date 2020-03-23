f14c7bc4976bab2221813ab4c8cd66bb8bcf822e
Constant Dupuis <31339604+constantdupuis@users.noreply.github.com>
2020-03-23 15:23:07 +0100

    Initial code commit
    Contains:
    - Docker COmpose file to start an instance of InfluxDB, Grafana & Chronograf
    - A starter app in dotnet to read write data to the influxdb

---
+++ dotnetInfluxdb/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading.Tasks;
+using Vibrant.InfluxDB.Client;
+
+namespace dotnetInfluxdb
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      var client = new InfluxClient(new Uri("http://localhost:8086"), "admin", "toto");
+      //Task < InfluxResult < DatabaseRow >> = 
+      var databases = client.ShowDatabasesAsync();
+      databases.Wait();
+      var dbs = databases.Result;
+      Console.WriteLine(dbs.ToString());
+    }
+  }
+}
